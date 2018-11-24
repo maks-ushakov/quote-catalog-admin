6676e12d3105f9c8a7580ac0f402e7db465d5b60
tbhaxor <tbhaxor@gmail.com>
2018-11-25 00:13:25 +0530

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
 		<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-			<router-link to="/" class="navbar-brand">Quote</router-link>
+			<router-link :to="{name:'home'}" class="navbar-brand">Quote</router-link>
 			<button
 				class="navbar-toggler"
 				type="button"
@@ -14,30 +14,54 @@
 				<span class="fas fa-bars"></span>
 			</button>
 			<div class="collapse navbar-collapse" id="navbarCollapse">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">
-							Home
-							<span class="sr-only">(current)</span>
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="#">Disabled</a>
+				<ul class="navbar-nav ml-auto">
+					<router-link
+						:to="{name:'home'}"
+						class="nav-link"
+						:exact="true"
+						exact-active-class="active"
+					>Home</router-link>
+					<router-link
+						:to="{name:'login'}"
+						exact
+						exact-active-class="active"
+						v-show="!logged"
+						class="nav-link"
+					>Login</router-link>
+					<li class="nav-item dropdown" v-show="logged">
+						<a
+							class="nav-link dropdown-toggle"
+							data-toggle="dropdown"
+							href="#"
+							role="button"
+							aria-haspopup="true"
+							aria-expanded="false"
+						>Dropdown</a>
+						<div class="dropdown-menu dropdown-menu-right">
+							<a class="dropdown-item" href="#">Action</a>
+							<a class="dropdown-item" href="#">Another action</a>
+							<a class="dropdown-item" href="#">Something else here</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Separated link</a>
+						</div>
 					</li>
 				</ul>
-				<form class="form-inline mt-2 mt-md-0">
-					<input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</form>
 			</div>
 		</nav>
-		<div class="container-fluid" style="margin-top:30px"></div>
+		<div class="container">
+			<router-view></router-view>
+		</div>
 	</section>
 </template>
 <script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      logged: false
+    };
+  }
+};
 </script>
 <style scoped>
 </style>
