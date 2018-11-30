4bb8ab1f1974e9c30067b73f99ca5a7d57684377
tbhaxor <tbhaxor@gmail.com>
2018-11-30 05:31:01 +0530

    added restore pass component

---
+++ src/components/restore-password.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <form action="" @submit.prevent="submit()">
+        <input placeholder="new password">
+        <input placeholder="confirm new password">
+        <button type="submit">Submit</button>
+        </form>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            token: this.$route.params.token
+        }
+    },
+    methods: {
+        submit(){
+            console.log("Submit")
+        }
+    },
+    created() {
+        // here we will check if token exists or not ?
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
