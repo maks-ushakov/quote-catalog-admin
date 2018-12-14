e6c797cf5af81d070df7ec5fdd12a5142b174190
tbhaxor <tbhaxor@gmail.com>
2018-12-14 17:21:37 +0530

    added login checks

---
+++ src/components/user/settings.vue
@@ -0,0 +1,23 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    fetch("/api/author/is-logged")
+      .then(resp => {
+        if (!resp.ok) {
+          this.$router.push({ name: "not-found" });
+        }
+      })
+      .catch(alert);
+  }
+};
+</script>
+
+<style>
+</style>
